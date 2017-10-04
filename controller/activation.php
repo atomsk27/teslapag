6a684b02592cdf9b4909a89e128dc0945e57ee8a
Unknown <bryanmmr@gmail.com>
2017-10-04 11:02:51 -0500

    eliminar pass

--- controller/activation.php
+++ controller/activation.php
@@ -1,12 +1,6 @@
 <?php
-    /**/
-    $db_host="teslaeducationsteam.org";
-    $db_user="teslaeducationst";
-    $db_password="5a4d6g8w7t5b4c2";
-    $db_name="teslaeducationsteam";
-    /**/
 
-    /*
+    /**/
     $db_host="localhost";
     $db_user="root";
     $db_password="rootroot";
