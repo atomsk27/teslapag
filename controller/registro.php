5e1dfad0c7522e2bc87a82ceebaf40b15d354654
Bryan Manzano <bryanmmr@gmail.com>
2017-10-23 17:32:06 -0500

    uniqid lenght

--- controller/registro.php
+++ controller/registro.php
@@ -28,7 +28,17 @@
     $subs_celularPadre = utf8_decode($_POST['celularPadre']);
 
     $subs_activo = 0;
-    $subs_aleatorio = uniqid();
+    function uniqidReal($lenght = 6){
+        if (function_exists("random_bytes")) {
+            $bytes = random_bytes(ceil($lenght / 2));
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes(ceil($lenght / 2));
+        } else {
+            throw new Exception("no cryptographically secure random function available");
+        }
+        return substr(bin2hex($bytes), 0, $lenght);
+    }
+    $subs_aleatorio = uniqidReal();
 
     $resultado = mysqli_query($db_connection, "SELECT * FROM ".$db_table_name." WHERE email = '".$subs_email."'");
 
