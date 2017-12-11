3c94fdb6d2d7dc34d276b81a90301a798a9f68e4
Bryan Manzano <bryanmmr@gmail.com>
2017-12-10 21:32:49 -0500

    check_login

--- controller/login.php
+++ controller/login.php
@@ -1,9 +1,49 @@
 <?php
     session_start();
+    require_once 'connection.php';
     require_once 'rolControl/Role.php';
     require_once 'rolControl/User.php';
-    require_once 'connection.php';
 
+    //SESSION CONTROL
+    $conexion = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+    if($conexion->connect_error){
+        die('La conexion fallo: '.$conexion->connect_error);
+    }
+
+    $username = $_POST['UserEmail'];
+    $password = $_POST['UserPass'];
+
+    $query = 'SELECT user, password FROM Usuario WHERE user = "'.$username.'"';
+
+    $result = $conexion->query($query);
+
+    if($result->num_rows > 0)
+    {}
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        
+        //corregir mysql a password_hash
+
+        if(password_verify($password, $row['password'])){
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+            $_SESSION['start'] = time();
+            $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
+
+            echo 'Bienvenido' . $_SESSION['username'];
+            echo "<br><br><a href='../dashboard/home.html'>Panel de control</a>";
+        }
+        else {
+            echo 'Username o pass incorrectos';
+
+            echo "<br><br><a href='../dashboard/'>Volver a intentarlo</a>";
+        }
+
+    mysqli_close($conexion);
+
+
+    //Rol control
+    /*
     if(isset($_SESSION['UserEmail']))
     {
         $user = new User($_SESSION['user_id']);
@@ -18,5 +58,5 @@
             //acciones que no puede hacer
         }
     }
-
+*/
  ?>
--- controller/rolControl/User.php
+++ controller/rolControl/User.php
@@ -1,41 +1,44 @@
 <?php
-    private $userRoles = array();
-    private static $db;
-
-    public static function setDatabase($db)
+    class User
     {
-        self::$db = $db;
-    }
+        private $userRoles = array();
+        private static $db;
 
-    public function __construct($user_id){
-        $getUser = $this->db->prepare('SELECT * FROM Usuario WHERE idUsuario = :user_id');
-        $getUser->execute(array('_user_id'=>$user_id));
-        if ($getUser->rowCount() == 1) {
-            $userData = $getUser->fetch (PDO::FETCH_ASSOC);
-            $this->user_id = $user_id;
-            $this->username = ucfirst($userData['user']);
-            $this->password = ucfirst($userData['pass']);
-            self::loadRoles();
-        }
-    protected static function loadRoles()
-    {
-        $fetchRoles = $this->db->prepare('SELECT Usuario.idUsuario, Usuario.idRol, Rol.nombreRol FROM Usuario JOIN Rol ON Rol.idRol = Usuario.idRol WHERE Usuario.idUsuario = :user_id');
-        $fetchRoles->execute(array(':user_id'=> $this->user_id));
-
-        while ($row = $fetchRoles->fetch(PDO::FETCH_ASSOC))
+        public static function setDatabase($db)
         {
-            $this->userRoles[$row['nombreRol']] = Role::getRolePermissions($row['idRol']);
+            self::$db = $db;
         }
-    }
-    public function hasPermission($permission)
-    {
-        foreach ($this->userRoles as $role) {
-            if($role->hasPermission($permission))
-            {
-                return true;
+
+        public function __construct($user_id){
+            $getUser = $this->db->prepare('SELECT * FROM Usuario WHERE idUsuario = :user_id');
+            $getUser->execute(array('_user_id'=>$user_id));
+            if ($getUser->rowCount() == 1) {
+                $userData = $getUser->fetch (PDO::FETCH_ASSOC);
+                $this->user_id = $user_id;
+                $this->username = ucfirst($userData['user']);
+                $this->password = ucfirst($userData['pass']);
+                self::loadRoles();
             }
         }
-        return false;
-    }
-    }
+        protected static function loadRoles()
+        {
+            $fetchRoles = $this->db->prepare('SELECT Usuario.idUsuario, Usuario.idRol, Rol.nombreRol FROM Usuario JOIN Rol ON Rol.idRol = Usuario.idRol WHERE Usuario.idUsuario = :user_id');
+            $fetchRoles->execute(array(':user_id'=> $this->user_id));
+
+            while ($row = $fetchRoles->fetch(PDO::FETCH_ASSOC))
+            {
+                $this->userRoles[$row['nombreRol']] = Role::getRolePermissions($row['idRol']);
+            }
+        }
+        public function hasPermission($permission)
+        {
+            foreach ($this->userRoles as $role) {
+                if($role->hasPermission($permission))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        }
  ?>
