4401aba0168df34e7f7baf008115dbd37f32b1da
Bryan Manzano <bryanmmr@gmail.com>
2017-12-12 13:09:56 -0500

    username capitalize

--- dashboard/home.php
+++ dashboard/home.php
@@ -36,13 +36,17 @@
 		<div class="full-box dashboard-sideBar-ct">
 			<!--SideBar Title -->
 			<div class="full-box text-uppercase text-center text-titles dashboard-sideBar-title">
-				company <i class="zmdi zmdi-close btn-menu-dashboard visible-xs"></i>
+				TESLA Education <i class="zmdi zmdi-close btn-menu-dashboard visible-xs"></i>
 			</div>
 			<!-- SideBar User info -->
 			<div class="full-box dashboard-sideBar-UserInfo">
 				<figure class="full-box">
 					<img src="./assets/img/avatar.jpg" alt="UserIcon">
-					<figcaption class="text-center text-titles">User Name</figcaption>
+					<figcaption class="text-center text-titles" style="text-transform:capitalize;">
+						<?php
+							echo $_SESSION['username'];
+						 ?>
+					</figcaption>
 				</figure>
 				<ul class="full-box list-unstyled text-center">
 					<li>
